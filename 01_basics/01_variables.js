6b3438c3b0b46011024aca45a9dda3f2f231c8a7
piyush <piyushjmj007@gmail.com>
2025-12-10 21:01:12 +0000

    config variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const AccoutId= 12345 //constant cannot be changed
+let AccountState="Delhi"
+var AccountEmail="hello@gmail.com"
+AccountPass="3456"
+
+
+/*
+always use let because of block scoping and functional scoping 
+*/
+console.table([AccoutId,AccountEmail,AccountState, AccountPass]);
