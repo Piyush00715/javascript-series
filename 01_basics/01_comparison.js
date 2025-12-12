e24428263aa664916f174df08c75ed986cafb85a
piyush <piyushjmj007@gmail.com>
2025-12-12 11:10:34 +0000

    datatype

---
+++ 01_basics/01_comparison.js
@@ -0,0 +1,2 @@
+//== looseequality operater
+//=== strict equality 
---
+++ 01_basics/01_datatypeconversion.js
@@ -0,0 +1,17 @@
+let num=33
+console.log(typeof num)
+
+let num1="33"
+stringToNum= Number(num1)
+console.log(typeof stringToNum)
+console.log(stringToNum)
+
+// "33" => 33
+// "33abc" => NaN(not a number)
+// null=>0
+//true =>1 && false =>0 and vice versa
+
+//****************operations*******************
+
+let value=3
+
