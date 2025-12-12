b259e8eb07f95300734c3fe4521b3d711b530565
piyush <piyushjmj007@gmail.com>
2025-12-12 11:24:50 +0000

    summary

---
+++ 01_basics/01_datatypesummary.js
@@ -0,0 +1,32 @@
+/*js is a dynamic typed language
+ # primitive datatypes
+
+-string
+-number
+-boolean
+-null
+-undefined
+-symbol
+-BigInt
+
+#reference type(non-primitive)-can be directly allocated in memory
+
+-array, objects, functions
+
+
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+       
+       */
