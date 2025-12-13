7180c94a593c01d732bd1d6ad14b0acbfac9b86f
piyush <piyushjmj007@gmail.com>
2025-12-13 05:04:41 +0000

    strings

--- 01_basics/01_datatypesummary.js
+++ 01_basics/01_datatypesummary.js
@@ -7,12 +7,25 @@
 -null
 -undefined
 -symbol
--BigInt
+-BigInt(const BigNumber=12345736348488n )
+
 
 #reference type(non-primitive)-can be directly allocated in memory
 
 -array, objects, functions
+array-example
+let frutis=[apple,banana,orange,mango]
 
+object-example
+let myobj={
+    name:"piyush",
+    age:18,
+}
+
+function example
+const myFunction=function(){
+    console.loog("hello world");
+}
 
 Return type of variables in JavaScript
 1) Primitive Datatypes
@@ -30,3 +43,27 @@ Return type of variables in JavaScript
        Object  =>  object
        
        */
+
+//stack(primitive) here we get the copy of the value hence original value does not change, heap(non primitive)here we get reference of original value...original value does not change
+
+//stack example
+
+let myname="piyush"
+let anothername=myname
+
+anothername="ayush"
+console.log(myname);
+console.log(anothername);
+
+//heap example
+
+//using object
+let userone={
+       email: "user@gmail.com",
+       class: 10
+}
+
+let usertwo=userone;
+usertwo.email="piyush@gmail.com";
+console.log(userone.email)
+console.log(usertwo.email)
---
+++ 01_basics/01_strings.js
@@ -0,0 +1,3 @@
+const name="piyush";
+let age=20;
+console.log(`my name is ${name} and I am ${age} years old`);
